26ee094df65af8b2f0b3bf979e2e0312e1e9c04c
Nico Rotolo <rotolonico@gmail.com>
2014-10-31 21:19:25 +0100

    Introducing score.

--- js/logic.js
+++ js/logic.js
@@ -1,57 +1,119 @@
 window.onload = function() {
     // We have a div called 
     var output = document.getElementById("output");
+    
+    
+    //funzioni di vincita o perdita
+    var win = function() {
+        score = score +1; 
+        output.innerHTML = "Your score is now " + score;
+        again();
+    };    
+    
+    var lose = function() {
+            score = 0;
+        again();
+    };
+        
+var score = 0;
 
-    var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
+var again = function() {
+    var go = prompt ("Do you want to play again?");
+    if (go === "yes") {
+        var userChoice = prompt("Do you choose rock, paper or scissors?");
+    var computerChoice = Math.random();
+    if (computerChoice < 0.34) {
 	computerChoice = "rock";
 } else if(computerChoice <= 0.67) {
 	computerChoice = "paper";
 } else {
 	computerChoice = "scissors";
 }
-output.innerHTML = "Computer: " + computerChoice;
+    compare(userChoice, computerChoice);
+} else if (go === "no") {
+    output.innerHTML = "Your final score is " + score;
+    score = 0;
+    saveScore();
+} else {
+            output.innerHTML = "Please enter yes or no.";
+    again();
+}
+    }
+    
+    
 
 var compare = function(choice1,choice2) {
 if (choice1 === choice2) {
     output.innerHTML = "The result is a tie!";
     var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-compare(userChoice,computerChoice);
+    var computerChoice = Math.random();
+    again();
 }
 else if (choice1 === "rock") {
 
 if (choice2 === "scissors") {
-    output.innerHTML = "rock wins"; }
+    output.innerHTML = "The computer was scissors, so you won";
+win();
+}
     else {
-        output.innerHTML = "paper wins";}
+        output.innerHTML = "the computer was paper so you lose";
+    lose();
+    }
 
 } // fine funzione choice === rock
     
 else if (choice1 === "paper") {
 
 if (choice2 === "rock") {
-    output.innerHTML = "paper wins"; }
+    output.innerHTML = "the computer was rock, so you won";
+win();
+}
+    
     else {
-        output.innerHTML = "scissors wins";}
+        output.innerHTML = "the computer was scissors, so you lose";
+    lose();
+    }
 
 } //fine funzione choice === paper
     
 else if (choice1 === "scissors") {
 
 if (choice2 === "rock") {
-    output.innerHTML = "rock wins";}
+    output.innerHTML = "the computer was rock, so you lose";
+lose();
+}
     else {
-        output.innerHTML =  "scissors wins";}
+        output.innerHTML =  "the computer was paper, so you won";
+    win();
+    }
 
-} // fine funzione choice === scissors
+} else if (choice1 === "score?") {
+    output.innerHTML= "Your score now is " + score; }
+    
+    // fine funzione choice === scissors
     
 else {
     output.innerHTML = "Please write rock,scissors or paper"; }
-
+again()
 // non toccare, fine funzione!
 }
-compare(userChoice,computerChoice);
+
+
+
+
+
+// starta il gioco per la prima volta
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+    var computerChoice = Math.random();
+    if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+}
+    compare(userChoice, computerChoice);
+    
+
 }
 
