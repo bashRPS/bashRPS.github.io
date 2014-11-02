652953bdfc7433b6fd6943321478d09316225116
Nico Rotolo <rotolonico@gmail.com>
2014-11-02 17:36:23 +0100

    Added roundScore.

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,14 @@
+// TO DO:
+// ROUND:
+// vinci tutte e 3: score +3 e vuoi giocare di nuovo
+// vinci 2: score +2 e vuoi giocare di nuovo
+// vinci 1: score = 0 e perdi (hai perso :( )
+
+
 window.onload = function() {
+    var scoregame = 0
     var game = 0
+    var firstTime = true;
     // We have a div called
     var output = document.getElementById("output");
 
@@ -7,8 +16,8 @@ window.onload = function() {
     //funzioni di vincita o perdita
     var win = function() {
         game = game +1;
-        score = score +1;
-        output.innerHTML = "Your score is now " + score;
+        scoregame = scoregame +1;
+        output.InnerHTML = "You won the round " + game;
         if (game === 3) {
         roundEnded();
         } else { 
@@ -17,7 +26,6 @@ window.onload = function() {
     }
 
     var lose = function() {
-        score = 0;
         game = game+1;
         if (game === 3) {
         roundEnded();
@@ -30,6 +38,25 @@ var score = 0;
 
 var roundEnded = function() {
     game = 0;
+    if (scoregame <= 1) {
+        output.innerHTML = "Sorry, you lost :(";
+        scoregame=0;
+        score = 0;
+        quit();
+    } else if (scoregame === 2) {
+        score = score + 2;
+        output.InnerHTML = "Your score is now " + score;
+        scoregame=0;
+        play();
+    } else { score = score + 3;
+            output.InnerHTML = "Your score is now " + score;
+            scoregame=0;
+            play();
+           }
+}
+
+    
+    var play = function() {
     var go = prompt ("Do you want to play again?");
     if (go === "yes") {
         var userChoice = prompt("Do you choose rock, paper or scissors?");
@@ -45,12 +72,13 @@ var roundEnded = function() {
 } else if (go === "no") {
     output.innerHTML = "Your final score is " + score;
     score = 0;
-    saveScore();
+    quit();
 } else {
             output.innerHTML = "Please enter yes or no.";
-    roundEnded();
+    play();
 }
     }
+
 var again = function() {
     var userChoice = prompt("Do you choose rock, paper or scissors?");
     var computerChoice = Math.random();
@@ -118,13 +146,13 @@ lose();
 
 else {
     output.innerHTML = "Please write rock,scissors or paper."; }
-again()
+again();
 // non toccare, fine funzione!
 }
 
 
 
-
+if (firstTime = true) {
 
 // starta il gioco per la prima volta
 var userChoice = prompt("Do you choose rock, paper or scissors?");
@@ -136,6 +164,8 @@ var userChoice = prompt("Do you choose rock, paper or scissors?");
 } else {
 	computerChoice = "scissors";
 }
+    firstTime = false;
     compare(userChoice, computerChoice);
+} else {};
 
 }
